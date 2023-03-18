9f88802745f581bce7e35742690d6ac9b28b44b0
Rachel Tucker <rach.mello@hotmail.com>
2023-03-18 12:46:48 -0500

    add 'OverlayingImages' to home page

---
+++ src/components/OverlayingImages.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import '../CSS_Files/font.css'
+
+const styles2 = {
+    parent: {
+        position: 'relative',
+        top: '300px',
+        left: '70px',
+    },
+    image1: {
+        width: '500px',
+        position: 'absolute',
+        top: '0px',
+        left: '30px',
+  
+        
+    },
+    image2: {
+        width:'450px',
+        position: 'relative',
+        top: '75px',
+        bottom:'50px',
+        left: '150px',
+
+    },
+}
+
+
+var overlapImagesList =[];
+
+class OverlappingImages extends React.Component{
+    importAll(r) {
+        return r.keys().map(r);
+    };
+    componentWillMount() {
+        overlapImagesList = this.importAll(require.context('../SMBP Test/Overlaying Images', false, /\.(png|jpe?g|svg)$/));
+    }
+    render(){
+        return (
+            <div className="parent" style={styles2.parent}>
+                <h2 className='Quote1'>"I capture fleeting moments, <br></br>so they last a lifetime"</h2>
+                <img className="image1" src={overlapImagesList[0]} style={styles2.image1} alt="bottomimg" />
+                <img className="image2" src={overlapImagesList[1]} style={styles2.image2} alt="topimg" />
+            </div>
+        )
+    }
+}
+
+export default OverlappingImages;
+    
