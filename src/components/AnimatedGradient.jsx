a925180d7939e0d749b67580fa3a4ef5f019c4dd
Rachel Tucker <rach.mello@hotmail.com>
2023-03-18 11:31:49 -0500

    created animated logo

---
+++ src/components/AnimatedGradient.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class AnimatedGradient extends React.Component {
+    constructor (props) {
+      super(props);
+      this.state = {
+        animationClass: 'gradient'
+      }
+    } 
+}
+
+
